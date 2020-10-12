939c9725520dc9c4d70388e8d2c638372d50a5bc
dcorso21 <david.l.corson@gmail.com>
2020-10-12 12:07:45 -0400

    managing state in form component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// Importing Components
+import ToDo from "./components/todolist";
+import Form from "./components/form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputText, setInputText] = useState("");
+    return (
+        <div className="App">
+            <header>
+    <h1>David's ToDo List</h1>
+            </header>
+            <Form  setInputText = {setInputText}/>
+            <ToDo />
+        </div>
+    );
 }
 
 export default App;
