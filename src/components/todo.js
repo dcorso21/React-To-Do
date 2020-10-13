e5620fabd9c62524f8b2b40df4b4f3faa82277eb
dcorso21 <david.l.corson@gmail.com>
2020-10-12 20:39:10 -0400

    completed class

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,29 +5,36 @@ function Todo({ todo, todos, setTodos }) {
         setTodos(todos.filter((el) => el.id !== todo.id));
     }
     function completeHandler(e) {
-        setTodos(todos.map((el) => {
-            if (el.id === todo.id){
-                return {
-                    ...el, completed : !el.completed
+        setTodos(
+            todos.map((el) => {
+                if (el.id === todo.id) {
+                    return {
+                        ...el,
+                        completed: !el.completed,
+                    };
                 }
-            }
-            return el;
-        }));
+                return el;
+            })
+        );
     }
     function setTodoText(e) {
         console.log(e.target.innerHTML);
-        e.target.innerHTML = (
-            `<input type="text"/>`
-        )
+        e.target.innerHTML = `<input type="text"/>`;
     }
     return (
         <div className="todo">
-            <li onClick={setTodoText} className={`todo-item ${todo.completed? "completed": ""}`}>Add a task here</li>
-            <button onClick={completeHandler} className="complete-btn">
-                <i className="fas fa-check"></i>
+            <li
+                onClick={setTodoText}
+                className="todo-item"
+            >
+                <input className ={`${todo.completed ? "completed-task" :""}`} 
+                 type="text" placeholder="Add a Task"/>
+            </li>
+            <button onClick={completeHandler} className={`complete-btn ${todo.completed? "completed-icon" :"" }`}>
+                <i className="fas fa-check-circle"></i>
             </button>
             <button onClick={deleteHandler} className="trash-btn">
-                <i className="fas fa-trash"></i>
+                <i className="fas fa-trash-alt"></i>
             </button>
         </div>
     );
